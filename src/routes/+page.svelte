7250413bffc9e47b3249dd511b25d04f6d9e1960
CARLOS <carlos@example.com>
2023-03-28 15:56:25 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> pepepepe</p>
+<div class ='page'>
+
+    <img src='logo.jpg' alt=''/>
+
+    <h1>Torneo padel DAW</h1>
+    
+    <input />
+    
+    <button on:click={addJugador}>ADD</button>
+    
+    <p>{name}</p>
+
+</div>
+
+<script>
+    let name = 'Carlos';
+
+    function addJugador() {
+        name = 'Hector';
+    }
+</script>
+
+<style>
+    .page{
+        display: grid;
+        justify-content: center;
+    }
+</style>
