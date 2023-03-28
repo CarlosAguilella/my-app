3174a2ab81a0fe669792e0e167a59c43e7b5955d
CARLOS <carlos@example.com>
2023-03-28 18:44:40 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,50 @@
 <div class ='page'>
 
-<img src='logo.jpg' alt=''/>    
+    <img src='logo.jpg' alt=''/>    
     <h1>Torneo padel DAW</h1>
-    <input bind:value={nPlayer}/>
 
-    {#if faltanPlayers }
+    {#if nPlayersEstablecido === false}
+        <input bind:value={nPlayer}/>
+        <button on:click={establecerCantidad}>CANTIDAD JUGADORES</button>
+    {/if}
+
+    {#if faltanPlayers && nPlayersEstablecido}
         <input bind:value = {name}/>
-    
-        <button on:click={addJugador}>ADD a {name}</button>
+        <button on:click={addJugador}>ADD a {players.length+1} de {nPlayer}</button>
     {/if}
     
     
     
     {#each players as player}
-        <li>{player} <button on:click={() => remJugador(player)}>🚮🚮🚮</button></li>            
+        <li>{player} {ranking[player]}<button on:click={() => remJugador(player)}>🚮🚮🚮</button></li>            
     {/each}
 
-    <button disabled={faltanPlayers} on:click={generar}>GENERAR</button>
+    {#if !faltanPlayers}
+        <button on:click={generar}>GENERAR</button>
+    {/if}
     
     {#each partidos as p}
-        <li><button>{p.j1} - {p.j2}</button> vs <button>{p.j3} - {p.j4}</button></li>
+        <li><button on:click={() => sumar(p.j1, p.j2)}>{p.j1} - {p.j2}</button> vs <button on:click={() => sumar(p.j3, p.j4)}>{p.j3} - {p.j4}</button></li>
     {/each}
 
 </div>
 
 <script>
-    let name ='';
-    let nPlayer ='';
+    let name = '';
+    let nPlayer = '';
+    let nPlayersEstablecido = false;
 
-    let players = ['Hector','Carlos','Gerard','Alex','Josep','JP','Juan','Dolors'];
+    let players = []; // ['Hector','Carlos','Gerard','Alex','Josep','JP','Juan','Dolors'];
     let partidos = [];
-    let ranking = [];
+    let ranking = {'Hector':0, 'Carlos':0, 'Gerard':0, 'Alex':0,'Josep':0,'JP':0,'Juan':0,'Dolors':0};
 
-    $: faltanPlayers = players.length < nPlayer;
+    $: faltanPlayers = nPlayer === '' || players.length < nPlayer;
 
     function addJugador() {
         players.push(name);
         players = players;
+        ranking[name] = 0;
+        name = '';
     }
 
     function remJugador(playerToRemove){
@@ -64,18 +72,30 @@
 
         partidos = partidos;
     }
+
+    function sumar(a, b) {
+        if (ranking[a] === undefined) ranking[a] = 0;
+        if (ranking[b] === undefined) ranking[b] = 0;
+        ranking[a]++;
+        ranking[b]++;
+    }
+
+    function establecerCantidad(){
+        nPlayersEstablecido = true;
+    }
 </script>
 
 <style>
-    .page{
+    .page {
         display: grid;
-        justify-content: center;
+        justify-items: center;
+        font-family:'System UI', 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     }
-    li{
+
+    li {
         list-style-type: "🎾";
         background-color: rgb(89, 243, 148);
         font-size: 25px;
-        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
         padding: 0.6em;
         margin: 0.3em;
         box-shadow: 3px 3px 3px black;
@@ -85,5 +105,9 @@
     }
     input{
         font-size: 30px;
+    }
+    img {
+        width: 8em;
+        display: block;
     }
 </style>
